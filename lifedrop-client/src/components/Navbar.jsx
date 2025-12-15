9e21c040b898af2f8c99725ac48faa7fb3fadf47
suronjit02 <suronjit02@gmail.com>
2025-12-15 16:25:40 +0600

    navbar update dashboard button

--- lifedrop-client/src/components/Navbar.jsx
+++ lifedrop-client/src/components/Navbar.jsx
@@ -49,35 +49,23 @@ const Navbar = () => {
         <img className="h-10 " src="/lifedrop.jpeg" alt="LifeDrop" />
       </div>
       <div className="navbar-center hidden lg:flex w-100 max-w-100">
-        <label className="input border border-[#c6414c] focus-within:border-[#c6414c] focus-within:ring-0 focus-within:outline-none w-full">
-          <svg
-            className="h-[1em] opacity-50"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-          >
-            <g
-              strokeLinejoin="round"
-              strokeLinecap="round"
-              strokeWidth="2.5"
-              fill="none"
-              stroke="currentColor"
-            >
-              <circle cx="11" cy="11" r="8"></circle>
-              <path d="m21 21-4.3-4.3"></path>
-            </g>
-          </svg>
-
-          <input
-            type="search"
-            required
-            placeholder="Search"
-            className="outline-none focus:outline-none focus:ring-0"
-          />
-        </label>
+        <ul className="flex justify-center items-center gap-5 font-semibold">
+          <li>
+            <NavLink>Donation Requests</NavLink>
+          </li>
+          <li>
+            <NavLink>Search</NavLink>
+          </li>
+        </ul>
       </div>
       <div className="navbar-end gap-2">
+        <Link
+          to={"/dashboard"}
+          className="btn bg-white border-[#c6414c] text-primary"
+        >
+          Dashboard
+        </Link>
         <Link className="btn primary text-white">Login</Link>
-        <Link className="btn primary text-white">Register</Link>
       </div>
     </div>
   );
