3891321e372edc5801c33594326d7a719e40e5f4
suronjit02 <suronjit02@gmail.com>
2025-12-15 14:54:57 +0600

    moto section update

--- lifedrop-client/src/components/home/MotoSection.jsx
+++ lifedrop-client/src/components/home/MotoSection.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const MotoSection = () => {
   return (
-    <section className="max-w-6xl mx-auto py-30 pb-35">
-      <blockquote className="text-center text-3xl italic font-semibold md:px-30">
-        <br />
+    <section className="max-w-6xl mx-auto py-30 flex gap-15 flex-col md:flex-row px-2 md:px-20 items-center justify-center">
+      <blockquote className="text-center text-3xl italic font-semibold ">
         "Every drop matters. Every life counts. Making blood donation easier for
         everyone."
       </blockquote>
+      <img className="h-80" src="/donate.png" alt="donate blood" />
     </section>
   );
 };
