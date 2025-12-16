492eee2851b0af237d93d579d0f36b5a2b6d578e
suronjit02 <suronjit02@gmail.com>
2025-12-17 03:13:17 +0600

    backend structure

--- lifedrop-backend/index.js
+++ lifedrop-backend/index.js
@@ -1,1 +1,21 @@
-console.log("backend here");
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+
+app.get("/", (req, res) => {
+  res.send("LifeDrop Donation");
+});
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
