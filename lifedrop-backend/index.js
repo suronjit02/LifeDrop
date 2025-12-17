f9ba7acea02de2b29335e764a9eb0b08a469619f
suronjit02 <suronjit02@gmail.com>
2025-12-17 16:16:49 +0600

    env file remove from github

--- lifedrop-backend/index.js
+++ lifedrop-backend/index.js
@@ -8,10 +8,51 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+//------------------------------------------------------------------------
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.gotmti8.mongodb.net/?appName=Cluster0`;
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
 
+    const database = client.db("lifedrop");
+    const userCollections = database.collection("users");
+
+    // user collection
+    app.post("/users", async (req, res) => {
+      const userInfo = req.body;
+      userInfo.createdAt = new Date();
+      userInfo.role = "donor";
+      userInfo.status = "active";
+      const result = await userCollections.insertOne(userInfo);
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// --------------------------------------------------------------------
 
 app.get("/", (req, res) => {
   res.send("LifeDrop Donation");
