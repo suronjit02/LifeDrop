7752c181759c3af06ab39ee28901f62f8c12f1ee
suronjit02 <suronjit02@gmail.com>
2025-12-20 07:01:40 +0600

    navbar design responsive

--- lifedrop-client/src/components/Navbar.jsx
+++ lifedrop-client/src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { Link, NavLink } from "react-router";
 import { AuthContext } from "../provider/AuthProvider";
 import { HiOutlineUserCircle } from "react-icons/hi";
+import { RiMenu3Fill } from "react-icons/ri";
 
 const Navbar = () => {
   const { user, logOut } = useContext(AuthContext);
@@ -10,49 +11,8 @@ const Navbar = () => {
     logOut();
   };
   return (
-    <div className="navbar bg-base-100 shadow-sm px-0 md:px-30">
+    <div className="navbar bg-base-100 shadow-sm px-2 md:px-30">
       <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              {" "}
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />{" "}
-            </svg>
-          </div>
-          <ul
-            tabIndex="-1"
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-          >
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
-          </ul>
-        </div>
         <Link to={"/"}>
           <img className="h-10 " src="/lifedrop.jpeg" alt="LifeDrop" />
         </Link>
@@ -86,33 +46,44 @@ const Navbar = () => {
                 tabIndex="-1"
                 className="dropdown-content  bg-base-100 rounded-md z-1 min-w-55 p-2 shadow-sm"
               >
-                <div className="text-sm mt-4">
-                  <li>Name: {user.displayName}</li>
-                  <li>Email: {user.email}</li>
-                  <hr className="my-3" />
+                <div className="text-sm text-center mt-4">
                   <li>
                     <Link
                       to={"/dashboard"}
-                      className=" text-primary font-semibold mb-3"
+                      className=" btn hover:bg-amber-100 bg-amber-50 px-20 py-2 rounded-sm text-primary font-semibold border-none mb-2"
                     >
                       Dashboard
                     </Link>
                   </li>
+                  <li>
+                    <Link
+                      to={"/"}
+                      onClick={handleLogOut}
+                      className="btn hover:bg-amber-100 bg-amber-50 px-23 py-2 rounded-sm text-primary font-semibold border-none"
+                    >
+                      Logout
+                    </Link>
+                  </li>
+
+                  <hr className="my-3 " />
+
+                  <div className="text-center">
+                    <li className="font-semibold">{user.displayName}</li>
+                    <li>{user.email}</li>
+                  </div>
                 </div>
-                <li>
-                  <Link
-                    to={"/"}
-                    onClick={handleLogOut}
-                    className="text-primary font-semibold "
-                  >
-                    Logout
-                  </Link>
-                </li>
               </ul>
             </div>
-            <Link onClick={handleLogOut} className="btn primary text-white">
-              LogOut
-            </Link>{" "}
+
+            <div className="hidden md:flex">
+              <Link
+                to={"/"}
+                onClick={handleLogOut}
+                className=" btn primary  text-white font-semibold "
+              >
+                Logout
+              </Link>
+            </div>
           </>
         ) : (
           <Link to={"/login"} className="btn primary text-white">
