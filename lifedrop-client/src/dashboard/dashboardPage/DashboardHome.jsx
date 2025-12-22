1d329de413060b9fb0c971dbeed0461949a966c9
suronjit02 <suronjit02@gmail.com>
2025-12-22 18:13:12 +0600

    edit button path fix

--- lifedrop-client/src/dashboard/dashboardPage/DashboardHome.jsx
+++ lifedrop-client/src/dashboard/dashboardPage/DashboardHome.jsx
@@ -192,7 +192,7 @@ const DashboardHome = () => {
                           View
                         </Link>
                         <Link
-                          to={`/dashboard/edit-request/${req._id}`}
+                          to={`/dashboard/edit-donation-request/${req._id}`}
                           className="btn btn-xs btn-warning"
                         >
                           Edit
