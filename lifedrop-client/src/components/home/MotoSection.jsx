6146941eb189d2a2c6d9e6c71d0d8131e73c9cb2
Suronjit Sutradhar <suronjit02@gmail.com>
2026-07-18 23:45:15 +0600

    feat: round the corner img

--- lifedrop-client/src/components/home/MotoSection.jsx
+++ lifedrop-client/src/components/home/MotoSection.jsx
@@ -3,12 +3,12 @@ import React from "react";
 const MotoSection = () => {
   return (
     <section className="max-w-7xl mx-auto py-30 flex gap-15 flex-col md:flex-row px-2 sm:px-5 items-center justify-center">
-      <blockquote className=" young-serif-regular text-center text-3xl italic font-semibold ">
+      <blockquote className=" young-serif-regular text-center text-3xl italic  ">
         "Every <span className="text-primary">drop</span> matters. Every{" "}
         <span className="text-primary">life</span> counts. <br />
         Making blood donation easier for everyone."
       </blockquote>
-      <img className="h-80" src="/donate.png" alt="donate blood" />
+      <img className="h-80 rounded-xl" src="/donate.png" alt="donate blood" />
     </section>
   );
 };
