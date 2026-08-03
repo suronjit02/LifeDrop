c86d8b4500e010983115637bf42d27b2454aeb3d
Suronjit Sutradhar <suronjit02@gmail.com>
2026-08-03 19:48:26 +0600

    feat: paymentSuccess page redisign

--- lifedrop-client/src/components/PaymentSuccess.jsx
+++ lifedrop-client/src/components/PaymentSuccess.jsx
@@ -15,10 +15,32 @@ const PaymentSuccess = () => {
   }, [sessionId]);
 
   return (
-    <div>
-      {status === "loading" && <p>Verifying payment...</p>}
-      {status === "success" && <h1>✅ Donation Successful! ধন্যবাদ।</h1>}
-      {status === "failed" && <h1>❌ Something went wrong</h1>}
+    <div className="flex items-center justify-center min-h-screen">
+      {status === "loading" && (
+        <p className="text-xl font-semibold text-blue-500">
+          Verifying payment...
+        </p>
+      )}
+
+      {status === "success" && (
+        <div className="text-center bg-green-100 p-10 py-15 rounded-md">
+          <h1 className="text-3xl font-bold text-secondary">
+            ✅ Donation Successful!
+          </h1>
+          <p className="mt-4 text-gray-700">Thank you for your support! ❤️</p>
+        </div>
+      )}
+
+      {status === "failed" && (
+        <div className="text-center bg-red-100 p-10 py-15 rounded-md">
+          <h1 className="text-3xl font-bold text-primary">
+            ❌ Something went wrong
+          </h1>
+          <p className="mt-4 text-gray-700">
+            Please try again or contact support.
+          </p>
+        </div>
+      )}
     </div>
   );
 };
