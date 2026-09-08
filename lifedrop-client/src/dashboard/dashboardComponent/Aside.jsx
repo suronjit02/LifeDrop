8d8aad33ec0ee52893b93d0a856df473993c744f
Suronjit Sutradhar <suronjit02@gmail.com>
2026-09-09 01:46:47 +0600

    update: dashboard aside Ui design

--- lifedrop-client/src/dashboard/dashboardComponent/Aside.jsx
+++ lifedrop-client/src/dashboard/dashboardComponent/Aside.jsx
@@ -4,9 +4,10 @@ import { FaUserCircle } from "react-icons/fa";
 import { FaCodePullRequest, FaUsers } from "react-icons/fa6";
 import { ImProfile } from "react-icons/im";
 import { IoHomeOutline } from "react-icons/io5";
-import { MdAddToPhotos, MdDashboard, MdOutlineBorderAll } from "react-icons/md";
+import { MdAddToPhotos, MdOutlineBorderAll } from "react-icons/md";
 import { NavLink, Link } from "react-router";
 import { AuthContext } from "../../provider/AuthProvider";
+import logo from "/icon.png";
 
 const Aside = ({ isOpen, setOpen }) => {
   const { user, logOut, role } = useContext(AuthContext);
@@ -14,6 +15,9 @@ const Aside = ({ isOpen, setOpen }) => {
     `flex items-center gap-2 p-2 rounded ${
       isActive ? "bg-white text-[#05b4cd]" : "hover:bg-white/20"
     }`;
+
+  // console.log("User Information: ", user);
+  // console.log("User Role: ", role);
 
   return (
     <>
@@ -30,19 +34,20 @@ const Aside = ({ isOpen, setOpen }) => {
         ${isOpen ? "translate-x-0" : "-translate-x-full"}
         md:translate-x-0 transition-transform duration-300`}
       >
-        <div className="mb-6 flex  justify-between items-center">
-          <h2 className="text-xl font-bold  flex items-center gap-2">
-            <MdDashboard /> Dashboard
-          </h2>
-          <Link to={"/"}>
-            <img
-              className="h-6 rounded-xs "
-              src="/lifedrop.png"
-              alt="LifeDrop"
-            />
-          </Link>
-        </div>
+        {/* title */}
+        <Link
+          to={"/"}
+          className="mb-6 text-xl text-secondary font-bold flex items-center gap-2 bg-white border border-gray-300 p-2 rounded-sm"
+        >
+          <span>
+            <img className="h-8" src={logo} alt="LifeDrop Logo" />
+          </span>{" "}
+          <span>
+            Life<span className="text-primary">Drop</span>
+          </span>
+        </Link>
 
+        {/* nav links */}
         <div className="flex flex-col justify-between min-h-[calc(100vh-85px)]">
           <nav className="space-y-2">
             <NavLink to="/dashboard" end className={linkClass}>
@@ -78,24 +83,34 @@ const Aside = ({ isOpen, setOpen }) => {
             )}
           </nav>
 
-          <div className="flex items-center justify-between mt-6">
-            {user?.photoURL ? (
-              <div>
-                <img
-                  src={user.photoURL}
-                  alt="profile"
-                  className="w-10 h-10 rounded-full object-cover cursor-pointer border"
-                />
-                <h3 className="capitalize font-bold ">{role}</h3>
+          {/* down part */}
+          <div className="space-y-3 my-4">
+            {/* information */}
+            <div className="flex items-center gap-2 rounded-sm">
+              {user?.photoURL ? (
+                <div>
+                  <img
+                    src={user.photoURL}
+                    alt="profile"
+                    className="h-12 rounded-full object-cover cursor-pointer border"
+                  />
+                  <h3 className="capitalize font-bold ">{role}</h3>
+                </div>
+              ) : (
+                <FaUserCircle className="text-4xl" />
+              )}
+
+              {/* name and email */}
+              <div className="">
+                <p className="text-md font-bold">{user.displayName}</p>
+                <p className="text-sm ">{user.email}</p>
               </div>
-            ) : (
-              <FaUserCircle className="text-4xl" />
-            )}
-            <div className="">
-              <Link onClick={logOut} to="/" className="btn btn-sm">
-                Logout <CiLogout />
-              </Link>
             </div>
+
+            {/* logout button */}
+            <Link onClick={logOut} to="/" className="btn btn-sm w-full">
+              Logout <CiLogout />
+            </Link>
           </div>
         </div>
       </aside>
